d3b8554613bacbaaf6e7863b176740c7fd3298dd
Pedro Miguel <pedromrap@gmail.com>
2023-06-28 17:44:35 -0300

    chore: jest setup

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+module.exports = {
+  testPathIgnorePatterns: ['/node_modules'],
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup-tests.js'],
+  collectCoverage: true,
+  moduleNameMapper: {
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/.jest/__mocks__/fileMock.js',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy'
+  },
+  collectCoverageFrom: ['src/**/*.tsx', '!src/**/*.spec.tsx']
+}
