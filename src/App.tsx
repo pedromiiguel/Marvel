a3e0ac4ea9064abc025f8865c0dbbcd771f6b610
Pedro Miguel <pedromrap@gmail.com>
2023-07-03 16:26:12 -0300

    feat: add providers in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider } from 'styled-components'
+import { Router } from './router'
+import { queryClient } from './lib/queryClient'
 import { GlobalStyle } from './styles/global'
 import theme from './styles/theme'
-import { Router } from './router'
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Router />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
 export default App
