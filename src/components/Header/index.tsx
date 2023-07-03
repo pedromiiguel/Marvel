6f7ddce005d2e0552b28d178515cd0e33fc00fb0
Pedro Miguel <pedromrap@gmail.com>
2023-07-03 18:06:45 -0300

    feat: render search only when in home route

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,24 @@
+import { useLocation } from 'react-router-dom'
+import { ROUTES } from '../../constants/routes'
 import { Logo } from '../Logo'
 import { SearchInput } from '../SearchInput'
 
 import * as S from './styles'
 
 export function Header() {
+  const { pathname } = useLocation()
+
+  const showSearchInput = pathname === ROUTES.HOME
+
   return (
     <S.Header>
       <S.LogoWrapper>
         <Logo />
       </S.LogoWrapper>
 
-      <SearchInput placeholder="Procurando algum herói específico?" />
+      {showSearchInput && (
+        <SearchInput placeholder="Procurando algum herói específico?" />
+      )}
     </S.Header>
   )
 }
