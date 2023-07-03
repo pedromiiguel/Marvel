193aaedebd3417920645df391a8a25fbd7bb1b3e
Pedro Miguel <pedromrap@gmail.com>
2023-07-03 16:21:02 -0300

    test: add custom render

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -5,12 +5,16 @@ import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
 
 import theme from '../styles/theme'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '../lib/queryClient'
 
 const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
