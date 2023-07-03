876fd9b99e1f850de4c7dd55655ef691cfa0a8bc
Pedro Miguel <pedromrap@gmail.com>
2023-07-03 19:39:14 -0300

    feat: Implement Router component with routes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Home } from '../pages/Home'
+import { DefaultLayout } from '../layouts/DefaultLayout'
+import { Character } from '../pages/Character'
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<DefaultLayout />}> */}
-        <Route path="/" element={<Home />} />
-        {/* </Route> */}
+        <Route path="/" element={<DefaultLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/character/:slug" element={<Character />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
