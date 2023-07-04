e722317b3e19adb8f45b20199de1e18677fb02d3
Pedro Miguel <pedromrap@gmail.com>
2023-07-03 21:47:32 -0300

    fix: change http for https

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const BASE_URL = 'http://gateway.marvel.com/v1/public'
+export const BASE_URL = 'https://gateway.marvel.com/v1/public'
 
 export const api = axios.create({
   baseURL: BASE_URL
